0399878c99fdb619f9d01b04c708efbdf8317db8
mahsaabdollahzadeh <mahsaabdollahzadeh@mail.um.ac.ir>
2022-04-17 14:47:31 -0700

    create2app

--- cloud/cloud/settings.py
+++ cloud/cloud/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'centeral',
+    'authentication',
+    'dataanalysis',
 ]
 
 MIDDLEWARE = [
--- cloud/cloud/urls.py
+++ cloud/cloud/urls.py
@@ -20,4 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('centeral.urls')),
+    path('',include('authentication.urls')),
+    path('',include('dataanalysis.urls')),
 ]
